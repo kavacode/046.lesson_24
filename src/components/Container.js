e2418f305099e14a1de6bb0e202a5997639c0c1f
kava <codekava@gmail.com>
2024-01-04 20:46:57 +0200

    first commit

---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Container() {
+  return (
+    <div className="container">
+      <h2>Що таке React JS?</h2>
+		<p>React — це декларативна, ефективна і гнучка JavaScript-бібліотека, призначена для створення інтерфейсів користувача. Вона дозволяє компонувати складні інтерфейси з невеликих окремих частин коду — “компонентів”.</p>
+		<a href="https://uk.legacy.reactjs.org/">Вивчення React</a>
+		<img src='111.png' alt='reactJS' />
+    </div>
+  );
+}
+
+export default Container;
